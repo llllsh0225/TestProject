0ee1a8f63ecc3062988b7a2a9d4e4c634afc01b9
kjh <kjh456879@naver.com>
2020-07-07 17:58:46 +0900

    no message

--- src/main/java/tommy/spring/teststop/App.java
+++ src/main/java/tommy/spring/teststop/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World222!" );
-        System.out.println("hihihihihihihihi");
+        System.out.println("hellohello");
     }
 }
