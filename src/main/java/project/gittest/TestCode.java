e7eb0085feb13e2778748895962c55a11038e961
Soo Hyun <rkdtngus0209@gmail.com>
2020-07-07 16:21:34 +0900

    깃허브 테스트

--- src/main/java/project/gittest/TestCode.java
+++ src/main/java/project/gittest/TestCode.java
@@ -2,6 +2,7 @@ package project.gittest;
 
 public class TestCode {
 	public static void main(String[] args) {
-		System.out.println("오수현 작성함.");
+		System.out.println("�삤�닔�쁽 �옉�꽦�븿.");
+		System.out.println("Hi");
 	}
 }
